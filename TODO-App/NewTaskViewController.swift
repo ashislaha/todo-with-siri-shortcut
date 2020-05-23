281d7d95c08fab7992299440d9a5d57f57f85bc6
Ashis Laha <mail2ashislaha@gmail.com>
2020-05-23 22:44:41 +0530

    added a new task viewcontroller

---
+++ TODO-App/NewTaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewTaskViewController.swift
+//  TODO-App
+//
+//  Created by Ashis Laha on 23/05/20.
+//  Copyright © 2020 Ashis Laha. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
