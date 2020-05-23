c074d6d513765ac82cf9e0aba272d1f01cc8ff9f
Ashis Laha <mail2ashislaha@gmail.com>
2020-05-23 23:01:48 +0530

    linking UI elements in New Task VC

--- TODO-App/NewTaskViewController.swift
+++ TODO-App/NewTaskViewController.swift
@@ -8,9 +8,68 @@
 
 import UIKit
 
+enum TaskType {
+	case none
+	case listening
+	case playing
+	case studying
+	case coding
+	
+	func getTitle() -> String {
+		switch self {
+		case .none: return ""
+		case .listening: return "Listening"
+		case .playing: return "Playing"
+		case .studying: return "Studying"
+		case .coding: return "Coding"
+		}
+	}
+}
+
 class NewTaskViewController: UIViewController {
 
-    override func viewDidLoad() {
+	private var currentTask: TaskType = .none {
+		didSet {
+			taskTypeResult?.text = currentTask.getTitle()
+		}
+	}
+	
+	@IBOutlet weak var taskTypeResult: UILabel!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
     }
+	
+	@IBAction func tapChooseTask(_ sender: UIButton) {
+		
+		let alertController = UIAlertController(title: "Choose a task", message: "", preferredStyle: .actionSheet)
+		
+		let listenAction = UIAlertAction(title: "Listening", style: .default) { (action) in
+			self.currentTask = .listening
+			// add the task into model
+			
+		}
+		
+		let playingAction = UIAlertAction(title: "Playing", style: .default) { (action) in
+			self.currentTask = .playing
+			// add the task into model
+			
+		}
+		
+		let studyAction = UIAlertAction(title: "Studying", style: .default) { (action) in
+			self.currentTask = .studying
+			// add the task into model
+			
+		}
+		
+		let codingAction = UIAlertAction(title: "Coding", style: .default) { (action) in
+			self.currentTask = .coding
+			// add the task into model
+		}
+		let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+		
+		[listenAction, playingAction, studyAction, codingAction, cancelAction].forEach { alertController.addAction($0) }
+		present(alertController, animated: true, completion: nil)
+	}
+	
 }
